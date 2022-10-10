013a726cd5914c4597bb574a2bfa36a0de6e9388
R-omek <rmngrck97@gmail.com>
2022-10-10 20:35:59 +0300

    fix burger menu, theme switcher and adaptation

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,20 +1,8 @@
-
-
-console.log(
-    "\n 1. Вёрстка соответствует макету. Ширина экрана 768px +48 \n 2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15 \n 3. На ширине экрана 768рх и меньше реализовано адаптивное меню +22 \n");
-
-
-
-let menuItem = document.querySelectorAll('.menu_item');
+const BODY = document.querySelector('body')
+const FOOTER = document.getElementById('footer')
+let menuItem = document.querySelectorAll('.menu__item');
 let menuBtn = document.querySelector('#menu_toggle');
-let HTML = document.querySelector('html');
 
-for (let i = 0; i < menuItem.length; i++){
-    menuItem[i].addEventListener('click', function(){
-        menuBtn.checked = false;
-        HTML.style.scrollBehavior = 'smooth';
-    })
-}
 
 const portfolioBtnWinter = document.querySelector('.portfolio-btn-winter');
 const portfolioBtnSpring = document.querySelector('.portfolio-btn-spring');
@@ -52,67 +40,30 @@ portfolioBtnAutumn.addEventListener('click', () => {
 });
 
 const sunBtn = document.querySelector('.switch-theme');
-const switchBtn = document.querySelector('#switcher-of-theme');
+const switchBtn = document.getElementById('switcher-of-theme');
+const MENU_TOGGLE = document.getElementById('menu__toggle');
 const skills = document.querySelector('#skills');
 const portfolio = document.querySelector('#portfolio');
 const video = document.querySelector('#video');
 const price = document.querySelector('#price');
 const allText = document.querySelectorAll('.txt_color');
 
+
+
 sunBtn.addEventListener('click', () => {
-    if(switchBtn.checked){
-        switchBtn.checked = false;
-    }else{
-        switchBtn.checked = true;
+    if(switchBtn.checked) {
+        BODY.classList.add('white-bg')
+        sunBtn.style.backgroundImage = 'url(assets/img/night-icon.png)'
+    } else {
+        BODY.classList.remove('white-bg')
+        sunBtn.style.backgroundImage = 'url(assets/svg/carbon_sun.svg)'
     }
-    if(switchBtn.checked){
-        skills.style.backgroundColor = '#ffffff';
-        portfolio.style.backgroundColor = '#ffffff';
-        video.style.backgroundColor = '#ffffff';
-        price.style.backgroundColor = '#ffffff';
-        for(let i = 0; i < allText.length; i ++){
-            allText[i].style.color = '#BDAE82';
-        }
-    }else{
-        skills.style.backgroundColor = '#000000';
-        skills.style.color = '#ffffff';
-        portfolio.style.backgroundColor = '#000000';
-        portfolio.style.color = '#ffffff';
-        video.style.backgroundColor = '#000000';
-        video.style.color = '#ffffff'; 
-        price.style.backgroundColor = '#000000';
-        price.style.color = '#ffffff';
-    }
-     
 });
 
+const hideMenuBox = () => {
+    MENU_TOGGLE.checked = false
+    console.log(switchBtn.checked)
+}
 
-const enBtn = document.querySelector('.en');
-const ruBtn = document.querySelector('.ru');
-const items = document.querySelectorAll('[data-i18]');
-const ruKeys = Object.keys(i18Obj['ru']);
-const enKeys = Object.keys(i18Obj['en']);
 
-ruBtn.addEventListener('click', () => {
-    ruBtn.classList.add('active_lan');
-    enBtn.classList.remove("active_lan");
-    items.forEach(item => {
-        for(let i = 0; i < ruKeys.length; i++){
-            if (item.getAttribute('data-i18') === ruKeys[i]){
-                item.textContent = i18Obj["ru"][item.getAttribute('data-i18')];
-            }
-        }  
-    });
-});
 
-enBtn.addEventListener('click', () => {
-    enBtn.classList.add('active_lan');
-    ruBtn.classList.remove("active_lan");
-    items.forEach(item => {
-        for(let i = 0; i < enKeys.length; i++){
-            if (item.getAttribute('data-i18') === enKeys[i]){
-                item.textContent = i18Obj["en"][item.getAttribute('data-i18')];
-            }
-        }  
-    });
-});
